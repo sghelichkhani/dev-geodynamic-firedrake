5120ac2a49b027ea84e228a72530bbe6277aaab4
Sia Ghelichkhan <siavash.ghelichkhan@anu.edu.au>
2022-05-20 16:45:38 +1000

    Changing the name of my directory

---
+++ adjoint_tic/00_BoxExtruded_simple/02_high_ra_plume/cases/00_no_regularisation/00_optimisation.py
@@ -0,0 +1,382 @@
+"""
+    Adjoint Reconstruction - Using the classic way inputing parameters, instead of definiting methods for ROL.Algirithm() 
+"""
+from firedrake import *
+from mpi4py import MPI
+from firedrake.petsc import PETSc
+from lib_averaging import LayerAveraging
+from firedrake_adjoint import *
+from pyadjoint import MinimizationProblem, ROLSolver
+from pyadjoint.tape import no_annotations, Tape, set_working_tape
+import ROL 
+import time
+
+#########################################################################################################
+################################## Some important constants etc...: #####################################
+#########################################################################################################
+
+# Quadrature degree:
+dx = dx(degree=6)
+
+# logging.set_log_level(1)
+# logging.set_level(1)
+
+with CheckpointFile("State_SteadyState.h5", "r") as t_ic_checkpoint:
+    mesh = t_ic_checkpoint.load_mesh("firedrake_default_extruded")
+    final_state = t_ic_checkpoint.load_function(mesh, "Temperature")
+
+
+# Top and bottom ids, for extruded mesh
+top_id, bottom_id = 'top', 'bottom'
+left_id, right_id = 1, 2
+
+# spatial coordinates
+X = x, y = SpatialCoordinate(mesh)
+
+# setting up vertical direction
+y_abs = sqrt(y**2)
+yhat = as_vector((0, y)) / y_abs
+
+# Global Constants:
+max_num_timesteps = 40
+
+# Stokes related constants:
+Ra = Constant(1e7)  # Rayleigh Number
+
+# Below are callbacks relating to the adjoint solutions
+# (accessed through solve).
+# Not sure what the best place would be to initiate working tape!
+tape = get_working_tape()
+
+# Temperature related constants:
+delta_t = Constant(1e-7)  # Time-step
+kappa = Constant(1.0)  # Thermal diffusivity
+
+# Temporal discretisation - Using a Crank-Nicholson scheme
+# where theta_ts = 0.5.
+theta_ts = 0.5
+
+
+# Print function to ensure log output is only written on processor
+# zero (if running in parallel) ####
+def log(*args):
+    if mesh.comm.rank == 0:
+        PETSc.Sys.Print(*args)
+
+
+# Energy Equation Solver Parameters:
+energy_iterative = {
+    "mat_type": "aij",
+    "snes_type": "ksponly",
+    "ksp_type": "gmres",
+    "ksp_rtol": 1e-5,
+    # "ksp_converged_reason": None,
+    "pc_type": "sor",
+}
+
+stokes_iterative = {
+    "mat_type": "matfree",
+    "snes_type": "ksponly",
+    "ksp_type": "preonly",
+    # "ksp_converged_reason": None,
+    "pc_type": "fieldsplit",
+    "pc_fieldsplit_type": "schur",
+    "pc_fieldsplit_schur_type": "full",
+    "fieldsplit_0": {
+        "ksp_type": "cg",
+        "ksp_rtol": 1e-4,
+        # "ksp_converged_reason": None,
+        "pc_type": "python",
+        "pc_python_type": "firedrake.AssembledPC",
+        "assembled_pc_type": "gamg",
+        "assembled_pc_gamg_threshold": 0.01,
+        "assembled_pc_gamg_square_graph": 100,
+    },
+    "fieldsplit_1": {
+        "ksp_type": "fgmres",
+        "ksp_rtol": 1e-3,
+        # "ksp_converged_reason": None,
+        "pc_type": "python",
+        "pc_python_type": "firedrake.MassInvPC",
+        "Mp_ksp_rtol": 1e-5,
+        "Mp_ksp_type": "cg",
+        "Mp_pc_type": "sor",
+    }
+}
+
+# Geometry and Spatial Discretization:
+# Set up function spaces - currently using the P2P1 element pair :
+V = VectorFunctionSpace(mesh, "CG", 2)  # Velocity function space (vector)
+W = FunctionSpace(mesh, "CG", 1)  # Pressure function space (scalar)
+Q = FunctionSpace(mesh, "CG", 2)  # Temperature function space (scalar)
+
+# Set up mixed function space and associated test functions:
+Z = MixedFunctionSpace([V, W])
+N, M = TestFunctions(Z)
+Y = TestFunction(Q)
+
+# Set up fields on these function spaces - split into each component
+# so that they are easily accessible:
+z = Function(Z)  # a field over the mixed function space Z.
+u, p = split(z)     # can we nicely name mixed function space fields?
+
+# T advection diffusion equation Prerequisites
+# Initial condition, let's start with the final condition
+T_ic = Function(Q, name="T_IC")
+T_ic.interpolate(final_state)
+
+# Set up temperature field and initialise it with present day
+T_old = Function(Q, name="OldTemperature")
+T_old.assign(T_ic)
+
+T_new = Function(Q, name="Temperature")
+T_new.assign(T_old)
+
+# Temporal discretisation - Using a theta scheme:
+T_theta = theta_ts * T_new + (1-theta_ts) * T_old
+
+# Setup Equations
+# viscosiy
+mu = Constant(1.0)  # Constant viscosity
+
+
+# deviatoric stresses
+def tau(u):
+    return mu * (grad(u) + transpose(grad(u)))
+
+
+# Stokes in weak form
+F_stokes = inner(grad(N), tau(u)) * dx - div(N)*p * dx
+F_stokes += - (dot(N, yhat)*Ra*T_theta) * dx
+F_stokes += - div(u) * M * dx
+
+# Setting free-slip BC for top and bottom
+bcu_topbase = DirichletBC(Z.sub(0).sub(1), 0.0, (top_id, bottom_id))
+bcu_rightleft = DirichletBC(Z.sub(0).sub(0), 0.0, (left_id, right_id))
+
+# Setting Dirihlet BC for top and bottom of the temperature field
+bct_top = DirichletBC(Q, 0.0, (top_id))
+bct_base = DirichletBC(Q, 1.0, (bottom_id))
+
+# Pressure nullspace
+p_nullspace = MixedVectorSpaceBasis(Z, [Z.sub(0),
+                                     VectorSpaceBasis(constant=True)]
+                                    )
+
+# Generating near_nullspaces for GAMG:
+z_rotV = Function(V).interpolate(as_vector((-X[1], X[0])))
+nns_x = Function(V).interpolate(Constant([1., 0.]))
+nns_y = Function(V).interpolate(Constant([0., 1.]))
+V_near_nullspace = VectorSpaceBasis([nns_x, nns_y, z_rotV])
+V_near_nullspace.orthonormalize()
+Z_near_nullspace = MixedVectorSpaceBasis(Z, [V_near_nullspace, Z.sub(1)])
+
+
+def absv(u):
+    """Component-wise absolute value of vector for SU stabilisation"""
+    return as_vector([abs(ui) for ui in u])
+
+
+def beta(Pe):
+    """Component-wise beta formula Donea and Huerta
+     (2.47a) for SU stabilisation"""
+    return as_vector([1/tanh(Pei+1e-6) - 1/(Pei+1e-6) for Pei in Pe])
+
+
+# copying SU scheme from global gplates
+J = Function(TensorFunctionSpace(mesh, 'DQ', 1), name='Jacobian').interpolate(Jacobian(mesh))
+Pe = absv(dot(u, J)) / 2
+nubar = dot(Pe, beta(Pe))
+Y_SU = Y + nubar / dot(u, u) * dot(u, grad(Y))
+
+# Temperature, advection-diffusion equation
+F_energy = Y * ((T_new - T_old) / delta_t) * dx\
+    + Y_SU*dot(u,grad(T_theta)) * dx\
+    + dot(grad(Y),kappa*grad(T_theta)) * dx
+
+# Setup problem and solver objects so we can reuse (cache) solver setup
+stokes_problem = NonlinearVariationalProblem(F_stokes, z, bcs=[bcu_topbase, bcu_rightleft])
+stokes_solver = NonlinearVariationalSolver(stokes_problem,
+                                           solver_parameters=stokes_iterative,
+                                           appctx={"mu": mu},
+                                           nullspace=p_nullspace,
+                                           transpose_nullspace=p_nullspace,
+                                           near_nullspace=Z_near_nullspace
+                                           )
+
+energy_problem = NonlinearVariationalProblem(F_energy, T_new,
+                                             bcs=[bct_base, bct_top]
+                                             )
+energy_solver = NonlinearVariationalSolver(energy_problem,
+                                           solver_parameters=energy_iterative
+                                           )
+
+# Setting adjoint and forward callbacks, and control parameter
+control = Control(T_ic)
+
+# Now perform the time loop:
+for timestep in range(0, max_num_timesteps):
+    # Solve system - configured for solving non-linear systems,
+    # where everything is on the LHS (as above)
+    # and the RHS == 0.
+    stokes_solver.solve()
+
+    # Temperature system:
+    energy_solver.solve()
+
+    # Set T_old = T_new - assign the values of T_new to T_old
+    T_old.assign(T_new)
+
+# Initialise functional
+functional = assemble(0.5*(T_new - final_state)**2 * dx)
+
+
+class myReducedFunctional(ReducedFunctional):
+    def __init__(self, functional, controls, **kwargs):
+        super().__init__(functional, control, **kwargs)
+        self.fwd_cntr = 0
+        self.adj_cntr = 0
+
+    def __call__(self, values):
+        pre_time = time.time()
+        val = super().__call__(values)
+        self.fwd_cntr += 1
+        log(f"\tFWD # {self.fwd_cntr} call took {time.time() - pre_time}")
+        return val
+
+    def derivative(self):
+        pre_time = time.time()
+        deriv = super().derivative(options={})
+        self.adj_cntr += 1
+        log(f"\tADJ # {self.adj_cntr} call took {time.time() - pre_time}")
+        return deriv
+
+
+# Defining the object for pyadjoint
+reduced_functional = myReducedFunctional(functional, control)
+
+# Set up bounds, which will later be used to
+# enforce boundary conditions in inversion:
+T_lb = Function(Q, name="LB_Temperature")
+T_ub = Function(Q, name="UB_Temperature")
+T_lb.assign(0.0)
+T_ub.assign(1.0)
+
+# Optimise using ROL - note when doing Taylor test this can be turned off:
+minp = MinimizationProblem(reduced_functional, bounds=(T_lb, T_ub))
+
+
+class myStatusTest(ROL.StatusTest):
+    def __init__(self, params, vector):
+        super().__init__(params)
+
+        # Keep track of the vector that is being passed to StatusCheck
+        self.vector = vector
+        self.T_copy = Function(Q, name="Temperature")
+        self.opt_t_final_file = File(str('visual_%3.3i/opt_temperature_fin.pvd' %max_num_timesteps))
+        self.opt_t_init_file = File(str('visual_%3.3i/opt_temperature_int.pvd' %max_num_timesteps))
+
+        # A linear temperature profile from the surface to 
+        # the CMB, with a gaussian blob somewhere
+        self.T_ic_true = Function(Q, name="InitTemperature_Ref")
+        true_initial_state_file = DumbCheckpoint(str("../../forward/State_%3.3i" %(100-max_num_timesteps)) , mode=FILE_READ)
+        true_initial_state_file.load(self.T_ic_true,    'Temperature')
+        true_initial_state_file.close()
+
+    @no_annotations
+    def check(self, status):
+
+        log("\tFinal misfit: {}".format(assemble(0.5*(T_new.block_variable.checkpoint - final_state)**2 * dx)))
+        log("\tInitial misfit : {}".format(assemble(0.5*(self.vector.dat[0] - self.T_ic_true)**2 * dx)))
+        #log("regularisation: {}".format(assemble(0.5*beta*dot(grad(self.vector.dat[0]), grad(self.vector.dat[0])) * dx)))
+
+        # Writing out final condition
+        self.T_copy.assign(T_new.block_variable.checkpoint)
+        self.opt_t_final_file.write(self.T_copy, final_state)
+
+        ## Writing out initial condition
+        self.T_copy.assign(self.vector.dat[0])
+        self.opt_t_init_file.write(self.T_copy, self.T_ic_true)
+
+        return ROL.StatusTest.check(self, status)
+
+
+# This is the classic way
+params = {
+        'General': {
+              'Print Verbosity': 1,
+              'Output Level': 3,
+              'Krylov': {# These are needed for our solution of the hessian I guess
+                    "Iteration Limit": 10,
+                    "Absolute Tolerance": 1e-4,
+                    "Relative Tolerance": 1e-2,
+                    },
+              'Secant': {'Type': 'Limited-Memory BFGS',
+                         'Maximum Storage': 20,
+                         'Use as Hessian': True,
+                         "Barzilai-Borwein": 1},
+                    },
+        'Step': {
+           'Type': 'Trust Region',#'Line Search',
+           'Trust Region': {
+                "Lin-More":     {
+                    "Maximum Number of Minor Iterations": 10,
+                    "Sufficient Decrease Parameter":      1e-2,
+                    "Relative Tolerance Exponent":        1.0,
+                    "Cauchy Point": {
+                        "Maximum Number of Reduction Steps": 10,
+                        "Maximum Number of Expansion Steps": 10,
+                        "Initial Step Size":                 1.0,
+                        "Normalize Initial Step Size":       False,
+                        "Reduction Rate":                    0.1,
+                        "Expansion Rate":                    10.0,
+                        "Decrease Tolerance":                1e-8,
+                                    },
+                        "Projected Search": {
+                                "Backtracking Rate": 0.5,
+                                "Maximum Number of Steps": 20,
+                                            },
+                                },
+                "Subproblem Model":                     "Lin-More",#"Kelley-Sachs",
+                "Initial Radius":                       0.005,
+                "Maximum Radius":                       1e20,
+                "Step Acceptance Threshold":            0.05,
+                "Radius Shrinking Threshold":           0.05,
+                "Radius Growing Threshold":             0.9,
+                "Radius Shrinking Rate (Negative rho)": 0.0625,
+                "Radius Shrinking Rate (Positive rho)": 0.25,
+                "Radius Growing Rate":                  2.5,
+                "Sufficient Decrease Parameter":        1.e-2,
+                "Safeguard Size":                       100,
+                            },
+                },
+        'Status Test': {
+            'Gradient Tolerance': 0,
+            'Iteration Limit': 50,
+                        }
+        }
+
+
+rol_solver = ROLSolver(minp, params)
+params = ROL.ParameterList(params, "Parameters")
+status_test = myStatusTest(params, rol_solver.rolvector)
+
+secant = ROL.InitBFGS(20)
+rol_algorithm = ROL.LinMoreAlgorithm(params, secant)
+rol_algorithm.setStatusTest(status_test, False)
+
+with stop_annotating():
+    rol_algorithm.run(
+        rol_solver.rolvector,
+        rol_solver.rolobjective,
+        rol_solver.bounds # only if we have a bounded problem
+            )
+    optimal_ic = rol_solver.problem.reduced_functional.controls.delist(rol_solver.rolvector.dat)
+    # Save the optimal temperature_ic field 
+    ckpt_T_ic = DumbCheckpoint(str("T_ic_State_%3.3i" %(max_num_timesteps)),\
+            single_file=True, mode=FILE_CREATE,\
+                               comm=mesh.comm)
+    ckpt_T_ic.store(optimal_ic)
+    ckpt_T_ic.close()
+
+
---
+++ adjoint_tic/00_BoxExtruded_simple/02_high_ra_plume/lib_averaging.py
@@ -0,0 +1,113 @@
+# noqa: F405
+from firedrake import *  # noqa: F403
+import numpy as np
+import scipy.linalg
+
+
+class LayerAveraging:
+    """Manager to compute a vertical profile of horizontal layer averages"""
+    def __init__(self, mesh, r1d, cartesian=True, quad_degree=None):
+        """
+        :arg mesh: source mesh
+        :arg r1d:  array of either y coordinates, or radii self.r, at which depths
+                   the averages should be calculated
+        :kwarg cartesian: if cartesian, the depths are y values, if not they
+                          are radii
+        :kwarg quad_degree: quadrature degree for the integrals. If the depths
+                            do not exactly match layers in the source mesh, you
+                            may have to yank up this number to avoid a noisy
+                            result."""
+        self.mesh = mesh
+        x, y = SpatialCoordinate(mesh)
+        if cartesian:
+            self.r = y
+        else:
+            self.r = sqrt(x**2 + y**2)
+        if quad_degree is None:
+            self.dx = dx
+        else:
+            self.dx = dx(degree=quad_degree)
+
+        self.r1d = r1d
+        # symmetric tri-diagonal mass matrix stored as 2 rows
+        # first row contains main diagonal
+        # second row contains off diagonal (ignoring last entry)
+        self.mass = np.zeros((2, len(r1d)))
+        self.rhs = np.zeros(len(r1d))
+        self._assemble_mass()
+
+    def _assemble_mass(self):
+        """Assembles the mass matrix for the layer average projection"""
+        # compute main diagonal of mass matrix
+        rc = Constant(self.r1d[0])
+        rn = Constant(self.r1d[1])
+        rp = Constant(0.)
+        # radial P1 hat function between rp<self.r<rn with max. at rc
+        Phi = Max(Min((self.r-rp)/(rc-rp), (rn-self.r)/(rn-rc)), 0)
+        for i, rin in enumerate(self.r1d[1:]):
+            rn.assign(rin)
+            self.mass[0, i] = assemble(Phi**2*self.dx)
+            rp.assign(rc)
+            rc.assign(rn)
+        # at this point rp and rc=rn are now the last two vertical nodes
+        # final basis function increases from 0 to 1 between rp and rn,
+        # and is constant 1 for self.r>rn
+        Phi = Max(Min(1, (self.r-rp)/(rn-rp)), 0)
+        self.mass[0, -1] = assemble(Phi**2*self.dx)
+
+        # now compute off-diagonal
+        rp = Constant(self.r1d[0])
+        rn = Constant(self.r1d[1])
+        # overlapping product between two basis functions between rp<self.r<rn
+        overlap = Max((rn-self.r)/(rn-rp), 0) * Max((self.r-rp)/(rn-rp), 0) * self.dx
+        for i, rin in enumerate(self.r1d[1:]):
+            rn.assign(rin)
+            self.mass[1, i] = assemble(overlap)
+            rp.assign(rn)
+
+        # sum of mass matrix should equal domain volume
+        # (need to count off-diagonal twice!)
+        np.testing.assert_almost_equal(self.mass.sum() + self.mass[1, :].sum(),
+                                       assemble(Constant(1, domain=self.mesh)*self.dx))
+
+    def _assemble_rhs(self, T):
+        """Assembles the rhs for the layer averaging projection into self.rhs"""
+        rc = Constant(self.r1d[0])
+        rn = Constant(self.r1d[1])
+        rp = Constant(0.)
+        # radial P1 hat function between rp<self.r<rn with max. at rc
+        Phi = Max(Min((self.r-rp)/(rc-rp), (rn-self.r)/(rn-rc)), 0)
+        for i, rin in enumerate(self.r1d[1:]):
+            rn.assign(rin)
+            self.rhs[i] = assemble(Phi*T*self.dx)
+            rp.assign(rc)
+            rc.assign(rn)
+        # at this point rp and rc=rn are now the last two vertical nodes
+        # final basis function increases from 0 to 1 between rp and rn, and is constant 1 for self.r>rn
+        Phi = Max(Min(1, (self.r-rp)/(rn-rp)), 0)
+        self.rhs[-1] = assemble(Phi*T*self.dx)
+
+    def get_layer_average(self, T):
+        """Compute the layer average of Function T at the specified depths and return the array of averages"""
+        self._assemble_rhs(T)
+        return scipy.linalg.solveh_banded(self.mass, self.rhs, lower=True)
+
+    def extrapolate_layer_average(self, u, avg):
+        """Given an array of layer averages (such as returned by get_layer_average(), extrapolate these to Function u"""
+        rc = Constant(self.r1d[0])
+        rn = Constant(self.r1d[1])
+        rp = Constant(0.)
+        # radial P1 hat function between rp<self.r<rn with max. at rc
+        Phi = Max(Min((self.r-rp)/(rc-rp), (rn-self.r)/(rn-rc)), 0)
+        value = Constant(0.)
+        for a, rin in zip(avg[:-1], self.r1d[1:]):
+            value.assign(a)
+            rn.assign(rin)
+            u.interpolate(u + value*Phi)
+            rp.assign(rc)
+            rc.assign(rn)
+        # at this point rp and rc=rn are now the last two vertical nodes
+        # final basis function increases from 0 to 1 between rp and rn, and is constant 1 for self.r>rn
+        Phi = Max(Min(1, (self.r-rp)/(rn-rp)), 0)
+        value.assign(avg[-1])
+        u.interpolate(u + value*Phi)
