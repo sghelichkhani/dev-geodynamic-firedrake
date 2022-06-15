3a6fd6b25e91e3755e5447942a94dee91580697e
Sia Ghelichkhan <sghelichkhani@Sias-MacBook-Pro.local>
2022-06-15 12:04:35 +1000

    Some minro changes

--- adjoint_tic/00_BoxExtruded_simple/01_low_ra_sink/inverse/03_optimisation_ufunctional_weighting.py
+++ adjoint_tic/00_BoxExtruded_simple/01_low_ra_sink/inverse/03_optimisation_ufunctional_weighting.py
@@ -3,14 +3,14 @@
 """
 from re import I
 import sys
-sys.path.append('../../')
+sys.path.append('../')
 
 from firedrake import *
 from mpi4py import MPI
 from firedrake.petsc import PETSc
 from lib_averaging import LayerAveraging
 from firedrake_adjoint import *
-from pyadjoint import MinimizationProblem, ROLSolver
+from pyadjoint import MinimizationProblem, ROLSolver, ROLVector
 from pyadjoint.tape import no_annotations, Tape, set_working_tape
 import ROL 
 import time
@@ -24,7 +24,7 @@ dx = dx(degree=6)
 # logging.set_log_level(1)
 # logging.set_level(1)
 
-with CheckpointFile("../../Final_State.h5", "r") as T_ref_checkpoint:
+with CheckpointFile("../Final_State.h5", "r") as T_ref_checkpoint:
     mesh = T_ref_checkpoint.load_mesh("firedrake_default_extruded")
     final_state = T_ref_checkpoint.load_function(mesh, "Temperature")
     final_state.rename("final_state")
@@ -41,8 +41,9 @@ X = x, y = SpatialCoordinate(mesh)
 y_abs = sqrt(y**2)
 yhat = as_vector((0, y)) / y_abs
 
-# Global Constants:
-max_num_timesteps = 70
+
+# Global Constants:max_num_timesteps = 150
+simu_time = 0.0
 
 # Stokes related constants:
 Ra = Constant(1e6)  # Rayleigh Number
@@ -53,19 +54,59 @@ Ra = Constant(1e6)  # Rayleigh Number
 tape = get_working_tape()
 
 # Temperature related constants:
-delta_t = Constant(5e-6)  # Time-step
+delta_t = Constant(1e-6)  # Time-step
 kappa = Constant(1.0)  # Thermal diffusivity
 
-# Temporal discretisation - Using a Crank-Nicholson scheme
-# where theta_ts = 0.5.
+# Temporal discretisation - Using a Crank-Nicholson
+# scheme where theta_ts = 0.5.
 theta_ts = 0.5
 
 
-# Print function to ensure log output is only written on processor
-# zero (if running in parallel) ####
+# Print function to ensure log output is only written
+# on processor zero (if running in parallel) ####
 def log(*args):
     if mesh.comm.rank == 0:
         PETSc.Sys.Print(*args)
+
+# Energy Equation Solver Parameters:
+energy_iterative = {
+    "mat_type": "aij",
+    "snes_type": "ksponly",
+    "ksp_type": "gmres",
+    "ksp_rtol": 1e-5,
+    #"ksp_converged_reason": None,
+    "pc_type": "sor",
+}
+
+stokes_iterative = {
+     "mat_type": "matfree",
+     "snes_type": "ksponly",
+     "ksp_type": "preonly",
+     #"ksp_converged_reason": None,
+     "pc_type": "fieldsplit",
+     "pc_fieldsplit_type": "schur",
+     "pc_fieldsplit_schur_type": "full",
+     "fieldsplit_0": {
+         "ksp_type": "cg",
+         "ksp_rtol": 1e-5,
+         #"ksp_converged_reason": None,
+         "pc_type": "python",
+         "pc_python_type": "firedrake.AssembledPC",
+         "assembled_pc_type": "gamg",
+         "assembled_pc_gamg_threshold": 0.01,
+         "assembled_pc_gamg_square_graph": 100,
+     },
+    "fieldsplit_1": {
+        "ksp_type": "fgmres",
+        "ksp_rtol": 1e-4,
+        #"ksp_converged_reason": None,
+        "pc_type": "python",
+        "pc_python_type": "firedrake.MassInvPC",
+        "Mp_ksp_rtol": 1e-5,
+        "Mp_ksp_type": "cg",
+        "Mp_pc_type": "sor",
+    }
+}
 
 
 # Geometry and Spatial Discretization:
@@ -79,8 +120,8 @@ Z = MixedFunctionSpace([V, W])
 N, M = TestFunctions(Z)
 Y = TestFunction(Q)
 
-# Set up fields on these function spaces - split into each component
-# so that they are easily accessible:
+# Set up fields on these function spaces - split into each 
+# component so that they are easily accessible:
 z = Function(Z)  # a field over the mixed function space Z.
 u, p = split(z)     # can we nicely name mixed function space fields?
 
@@ -111,14 +152,14 @@ def tau(u):
 
 # Stokes in weak form
 F_stokes = inner(grad(N), tau(u)) * dx - div(N)*p * dx
-F_stokes += - (dot(N, yhat)*Ra*T_theta) * dx
+F_stokes += - (dot(N, yhat) * Ra * T_theta) * dx
 F_stokes += - div(u) * M * dx
 
 # Setting free-slip BC for bottom, and sides
 u_surface = Function(V, name="BoundaryCondition")
 bcu_topbase = DirichletBC(Z.sub(0).sub(1), 0.0, (bottom_id, top_id))
 bcu_rightleft = DirichletBC(Z.sub(0).sub(0), 0.0, (left_id, right_id))
-all_u_bounds = [bcu_topbase, bcu_rightleft]
+all_bcu = [bcu_topbase, bcu_rightleft]
 
 # Setting Dirihlet BC for top and bottom of the temperature field
 bct_top = DirichletBC(Q, 0.0, (top_id))
@@ -126,11 +167,11 @@ bct_base = DirichletBC(Q, 1.0, (bottom_id))
 
 # Pressure nullspace
 p_nullspace = MixedVectorSpaceBasis(Z, [Z.sub(0),
-                                     VectorSpaceBasis(constant=True)]
+                                    VectorSpaceBasis(constant=True)]
                                     )
 
 # reference velocity file
-u_ref_file = CheckpointFile("../../Ref_velocities.h5", mode='r')
+u_ref_file = CheckpointFile("../Ref_velocities.h5", mode='r')
 
 # Temperature, advection-diffusion equation
 F_energy = Y * ((T_new - T_old) / delta_t) * dx\
@@ -138,15 +179,12 @@ F_energy = Y * ((T_new - T_old) / delta_t) * dx\
     + dot(grad(Y),kappa*grad(T_theta)) * dx
 
 # Setup problem and solver objects so we can reuse (cache) solver setup
-stokes_problem = NonlinearVariationalProblem(F_stokes, z, bcs=all_u_bounds)
+stokes_problem = NonlinearVariationalProblem(F_stokes, z, bcs=all_bcu)
 stokes_solver = NonlinearVariationalSolver(stokes_problem,
-                                           nullspace=p_nullspace,
-                                           transpose_nullspace=p_nullspace,
-                                           )
+            nullspace=p_nullspace, transpose_nullspace=p_nullspace
+)
 
-energy_problem = NonlinearVariationalProblem(F_energy, T_new,
-                                             bcs=[bct_base, bct_top]
-                                             )
+energy_problem = NonlinearVariationalProblem(F_energy, T_new, bcs=[bct_base, bct_top])
 energy_solver = NonlinearVariationalSolver(energy_problem)
 
 # splitting fields just to have access
@@ -156,6 +194,9 @@ p_.rename("Pressure")
 
 # Setting adjoint and forward callbacks, and control parameter
 control = Control(T_ic)
+
+# Make sure boundary conditions are applied to the control
+solve(Y*TrialFunction(Q)*dx == Y*T_ic*dx, T_ic, bcs=[bct_base, bct_top])
 
 u_functional = 0
 
@@ -184,21 +225,49 @@ temp_functional = assemble(0.5*(T_new - final_state)**2 * dx)/assemble(0.5*(fina
 
 
 class myReducedFunctional(ReducedFunctional):
-    def __init__(self, functional, controls, **kwargs):
+    def __init__(self, functional, controls, fname=None, **kwargs):
         super().__init__(functional, control, **kwargs)
         self.fwd_cntr = 0
         self.adj_cntr = 0
 
+        self.fname = None
+
+        if fname:
+            self.fname = fname
+            self.derfile = File(fname.replace(".pvd", "_der.pvd"))
+            self.derFunction = Function(Q, name="gradient")
+            self.derRol = ROLVector(self.derFunction, inner_product="L2")
+
+
+            self.valuefile = File(fname.replace(".pvd", "_ctrl.pvd"))
+            self.valuefunction = Function(Q, name="control") 
+
+            self.valueCheckPoint = CheckpointFile(fname.replace(".pvd", ".h5"), 'w')
+            self.valueCheckPoint.save_mesh(mesh)
+
+
     def __call__(self, values):
+        # writing the control value to file
+        if self.fname:
+            self.valuefunction.interpolate(values[0])
+            self.valuefile.write(self.valuefunction)
+            self.valueCheckPoint.save_function(self.valuefunction, idx=self.fwd_cntr)
+
         pre_time = time.time()
         val = super().__call__(values)
         self.fwd_cntr += 1
         log(f"\tFWD # {self.fwd_cntr} call took {time.time() - pre_time}")
-        return val
+        return val 
 
     def derivative(self):
         pre_time = time.time()
-        deriv = super().derivative(options={})
+        deriv = super().derivative()
+
+        if self.fname:
+            self.derRol.dat = self.derRol.riesz_map(deriv)
+            self.derFunction.interpolate(self.derRol.dat[0])
+            self.derfile.write(self.derFunction)
+
         self.adj_cntr += 1
         log(f"\tADJ # {self.adj_cntr} call took {time.time() - pre_time}")
         return deriv
@@ -236,7 +305,7 @@ class myStatusTest(ROL.StatusTest):
         self.solution_checkpoint.save_mesh(mesh)
 
         # loading the true answer for comparisons 
-        with CheckpointFile("../../Initial_State.h5", 'r') as true_initial_state_file:
+        with CheckpointFile("../Initial_State.h5", 'r') as true_initial_state_file:
             _ = true_initial_state_file.load_mesh("firedrake_default_extruded")
             self.T_ic_true = true_initial_state_file.load_function(mesh, "Temperature")
             self.T_ic_true.rename("ref_initial_state")
@@ -269,8 +338,8 @@ params = {
         'General': {
               'Print Verbosity': 1,
               'Output Level': 3,
-              'Krylov': {  # These are needed for our solution
-                           # of the hessian I guess
+              'Krylov': {   # These are needed for our
+                            # solution of the hessian I guess
                     "Iteration Limit": 10,
                     "Absolute Tolerance": 1e-4,
                     "Relative Tolerance": 1e-2,
@@ -301,9 +370,9 @@ params = {
                                 "Maximum Number of Steps": 20,
                                             },
                                 },
-                # "Kelley-Sachs",
+                #  Subproblem Model could be "Kelley-Sachs",
                 "Subproblem Model":                     "Lin-More",
-                "Initial Radius":                       0.005,
+                "Initial Radius":                       1.00,
                 "Maximum Radius":                       1e20,
                 "Step Acceptance Threshold":            0.05,
                 "Radius Shrinking Threshold":           0.05,
