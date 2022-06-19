2fbf251bf876384d07376496c318f7b0267f87a3
Sia Ghelichkhan <ghelichkhani.siavash@gmail.com>
2022-06-20 09:47:32 +1000

    Making the test fail

--- adjoint_tic/04_test_mesh/test.py
+++ adjoint_tic/04_test_mesh/test.py
@@ -1,25 +1,24 @@
 from firedrake import *
 
+# dimensions of the mesh
 y_max = 1.0
 x_max = 3.0
 
-#mesh = PeriodicRectangleMesh(450, 150, x_max, y_max, direction='x')
+# mesh generation
+mesh = PeriodicRectangleMesh(450, 150, x_max, y_max, direction='x')
 
-
-# and Interval mesh of unit size 
-mesh1d = IntervalMesh(100, length_or_left=0.0, right=x_max)
-
-# extruding the base mesh "mesh1d" in the third dimension
-mesh = ExtrudedMesh(mesh=mesh1d, layers=100, layer_height=y_max/100, extrusion_type='uniform', kernel=None, gdim=None)
-
+# function space 
 Q2 = FunctionSpace(mesh, "CG", 2)
-
+# function to map the distance from (0,0)
 a_field = Function(Q2, name='field')
 
+# UFL representation of coordinates
 X = x, y = SpatialCoordinate(mesh)
 
+# interpolating radius to function
 a_field.interpolate(sqrt(x**2+y**2))
 
+# writing out
 with CheckpointFile('a_checkpoint.h5', mode='w') as mesh_checkpoint:
     mesh_checkpoint.save_mesh(mesh)
     mesh_checkpoint.save_function(a_field, name='field')
@@ -29,8 +28,20 @@ with CheckpointFile('a_checkpoint.h5', mode='w') as mesh_checkpoint:
 with CheckpointFile('a_checkpoint.h5', mode='r') as mesh_checkpoint:
     reloaded_mesh = mesh_checkpoint.load_mesh()
 
-X_r = x_r, y_r = SpatialCoordinate(reloaded_mesh)
+# function space 
+Q2_r = FunctionSpace(reloaded_mesh, "CG", 2)
 
-a_field_r = Function(Q2, name="field_reloaded")
+X_r = x_r, y_r = SpatialCoordinate(reloaded_mesh)
+#x_r, y_r = X_r[0], X_r[1]
+
+
+a_field_r = Function(Q2_r, name="field_reloaded")
 a_field_r.interpolate(sqrt(x_r**2+y_r**2))
+
+
+pvd_file = File('test.pvd')
+pvd_file.write(a_field)
+
+pvd_file_r = File('test_r.pvd')
+pvd_file_r.write(a_field_r)
 
